16f0d2a339ecbe223d298c4c0d90dbd42781002c
sawepeter <sawepeter6@gmail.com>
2024-09-24 13:57:59 +0300

    navbar

--- src/app/(dashboard)/layout.tsx
+++ src/app/(dashboard)/layout.tsx
@@ -8,8 +8,8 @@ export default function DashboardLayout({
 }>) {
   return <div className="h-screen flex">
     {/* LEFT */}
-    <div className="w-[14%] md:w-[8%] lg:w-[16%] xl:w-[14%] bg-red-200">
-      <Link href="/" className="flex items-center justify-center gap-2">
+    <div className="w-[14%] md:w-[8%] lg:w-[16%] xl:w-[14%] bg-red-200 p-4">
+      <Link href="/" className="flex items-center justify-center lg:justify-start gap-2">
         <Image src="/logo.png" alt="logo" width={32} height={32} />
         <span className="hidden lg:block">Scholaro</span>
       </Link>
