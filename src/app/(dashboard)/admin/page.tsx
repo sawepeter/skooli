306f164fec6b6becb56e4b1e1283d4ebb8ab1f44
sawepeter <sawepeter6@gmail.com>
2024-09-24 15:31:14 +0300

    components(usercards)

--- src/app/(dashboard)/admin/page.tsx
+++ src/app/(dashboard)/admin/page.tsx
@@ -1,8 +1,18 @@
+import UserCard from "@/components/UserCard"
+
 const AdminPage = () => {
     return (
         <div className="p-4 flex gap-4 flex-col md:floex-row">
             {/* LEFT */}
-            <div className="w-full lg:w-2/3">l</div>
+            <div className="w-full lg:w-2/3">
+            {/* USER CARDS */}
+            <div className="flex gap-4 ">
+                <UserCard type="student"/>
+                <UserCard type="teacher"/>
+                <UserCard type="staff"/>
+                <UserCard type="admin"/>
+            </div>
+            </div>
             {/* RIGHT */}
             <div className="w-full lg:w-1/3">r</div>
         </div>
---
+++ src/components/UserCard.tsx
@@ -0,0 +1,10 @@
+import React from 'react'
+
+const UserCard = ({type}:{type:string}) => {
+  return (
+    <div className=''>
+        UserCard</div>
+  )
+}
+
+export default UserCard
