32f453a63db6d2fec9bca01848543d7b7db22eb0
sawepeter <sawepeter6@gmail.com>
2024-09-25 02:17:55 +0300

    dashboard design

--- src/app/(dashboard)/admin/page.tsx
+++ src/app/(dashboard)/admin/page.tsx
@@ -2,19 +2,19 @@ import UserCard from "@/components/UserCard"
 
 const AdminPage = () => {
     return (
-        <div className="p-4 flex gap-4 flex-col md:floex-row">
+        <div className="p-4 flex gap-4 flex-col md:flex-row">
             {/* LEFT */}
-            <div className="w-full lg:w-2/3">
+            <div className="w-full lg:w-2/3 flex flex-col gap-8">
             {/* USER CARDS */}
-            <div className="flex gap-4 ">
-                <UserCard type="student"/>
-                <UserCard type="teacher"/>
+            <div className="flex gap-4 justify-between flex-wrap">
+                <UserCard type="students"/>
+                <UserCard type="teachers"/>
                 <UserCard type="staff"/>
                 <UserCard type="admin"/>
             </div>
             </div>
             {/* RIGHT */}
-            <div className="w-full lg:w-1/3">r</div>
+            <div className="w-full lg:w-1/3"></div>
         </div>
     )
 }
--- src/app/(dashboard)/layout.tsx
+++ src/app/(dashboard)/layout.tsx
@@ -12,13 +12,14 @@ export default function DashboardLayout({
       {/* LEFT */}
       <div className="w-[14%] md:w-[8%] lg:w-[16%] xl:w-[14%] p-4">
         <Link href="/" className="flex items-center justify-center lg:justify-start gap-2">
-          <Image src="/logo.png" alt="logo" width={32} height={32} />
+          <Image src="/skooli.png" alt="logo" width={32} height={32} />
           <span className="hidden lg:block font-bold">Skooli</span>
         </Link>
       </div>
       {/* RIGHT */}
       <div className="w-[86%] md:w-[92%] lg:w-[84%] xl:w-[86%] bg-[#F7F8FA] overflow-scroll flex flex-col">
         <Navbar />
+        {children}
       </div>
     </div>
 
