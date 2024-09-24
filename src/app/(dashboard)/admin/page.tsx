affbed8c3f2aae4b0eb84634f99b914e7ab0ada2
sawepeter <sawepeter6@gmail.com>
2024-09-24 15:15:22 +0300

    intial commit

--- src/app/(dashboard)/admin/page.tsx
+++ src/app/(dashboard)/admin/page.tsx
@@ -1,6 +1,11 @@
 const AdminPage = () => {
     return (
-        <div className="">AdminPage</div>
+        <div className="p-4 flex gap-4 flex-col md:floex-row">
+            {/* LEFT */}
+            <div className="w-full lg:w-2/3">l</div>
+            {/* RIGHT */}
+            <div className="w-full lg:w-1/3">r</div>
+        </div>
     )
 }
 
