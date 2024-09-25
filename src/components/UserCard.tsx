aa790928f68375377505f240dbe205305b2ae6a5
sawepeter <sawepeter6@gmail.com>
2024-09-25 03:09:47 +0300

    recharts component

--- src/components/UserCard.tsx
+++ src/components/UserCard.tsx
@@ -11,7 +11,7 @@ const UserCard = ({ type }: { type: string }) => {
             <Image src="/more.png" alt="" width={20} height={20} />
         </div>
         <h1 className='text-2xl font-semibold my-4'>700</h1>
-        <h2 className='capitalize text-sm font-medium text-gray-500'>{type}</h2>
+        <h2 className="capitalize text-sm font-medium text-gray-500">{type}</h2>
     </div>
   );
 };
