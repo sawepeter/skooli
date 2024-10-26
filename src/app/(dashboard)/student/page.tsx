7b1c6ba9f152c6db7de50989bb54fcc2959381d3
sawepeter <sawepeter6@gmail.com>
2024-10-26 17:16:52 +0300

    bigCalendar component

--- src/app/(dashboard)/student/page.tsx
+++ src/app/(dashboard)/student/page.tsx
@@ -1,6 +1,6 @@
-import Announcements from "@/components/Announcements"
-import BigCalendar from "@/components/BigCalendar"
-import EventCalendar from "@/components/EventCalendar"
+import Announcements from "@/components/Announcements";
+import BigCalendar from "@/components/BigCalendar";
+import EventCalendar from "@/components/EventCalendar";
 
 const StudentPage = () => {
     return (
--- src/components/BigCalendar.tsx
+++ src/components/BigCalendar.tsx
@@ -1,10 +1,11 @@
 "use client";
 
-import { calendarEvents } from '@/lib/data';
 import React, { useState } from 'react'
-import { Calendar, momentLocalizer, View, Views } from "react-big-calendar";
+import moment from 'moment';
+import { Calendar, momentLocalizer, View, Views } from 'react-big-calendar';
+import { calendarEvents } from '@/lib/data';
 import "react-big-calendar/lib/css/react-big-calendar.css";
-import moment from "moment";
+
 
 const localizer = momentLocalizer(moment);
 
@@ -12,21 +13,22 @@ const BigCalendar = () => {
     const [view, setView] = useState<View>(Views.WORK_WEEK);
 
     const handleOnChangeView = (selectedView: View) => {
-        setView(selectedView);
+        setView(selectedView)
     };
 
   return (
-    <Calendar
-        localizer={localizer}
-        events={calendarEvents}
-        startAccessor="start"
-        endAccessor="end"
-        views={["work_week", "day"]}
-        view={view}
-        style={{ height: "98%" }}
-        onView={handleOnChangeView}
-        min={new Date(2025, 1, 0, 8, 0, 0)}
-        max={new Date(2025, 1, 0, 17, 0, 0)} />
+        <Calendar
+            localizer={localizer}
+            events={calendarEvents}
+            style={{ height: "98%" }}
+            startAccessor="start"
+            endAccessor="end"
+            views={[ "work_week", "day"]}
+            view={view}
+            onView={handleOnChangeView}
+            min={new Date(2025, 1, 0, 8, 0, 0)}
+            max={new Date(2025, 1, 0, 17, 0, 0)}
+        />
   );
 };
 
