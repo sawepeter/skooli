34e3f8443e915e9c2bd70232b94c50703f722c7e
sawepeter <sawepeter6@gmail.com>
2024-10-29 21:58:04 +0300

    Teacher and student forms

--- src/app/(dashboard)/list/teachers/[id]/page.tsx
+++ src/app/(dashboard)/list/teachers/[id]/page.tsx
@@ -1,6 +1,10 @@
+import Announcements from '@/components/Announcements';
+import BigCalendar from '@/components/BigCalendar';
 import FormModal from '@/components/FormModal';
+import Performance from '@/components/Performance';
 import { role } from '@/lib/data';
 import Image from 'next/image';
+import Link from 'next/link';
 import React from 'react'
 
 const SingleTeacherPage = () => {
@@ -95,17 +99,41 @@ const SingleTeacherPage = () => {
                     <div className=''>
                         <h1 className='text-xl font-semibold'>6</h1>
                         <span className='text-sm text-gray-400'>Classes</span>
-
                     </div>
-
                 </div>
-
             </div>
-
         </div>
-
+        {/* BOTTOM */}
+        <div className='mt-4 bg-white rounded-md p-4 h-[800px]'>
+            <h1>Teacher&apos;s Schedule</h1>
+            <BigCalendar />
         </div>
-        
+        </div>
+        {/* RIGHT */}
+        <div className='w-full xl:w-1/3 flex flex-col gap-4'>
+          <div className='bg-white p-4 rounded-md'>
+            <h1 className='text-xl font-semibold'>Shortcuts</h1>
+           <div className='mt-4 flex gap-4 flex-wrap text-xs text-gray-500'>
+             <Link className='p-3 rounded-md bg-lamaSkyLight' href="/">
+                Teacher&apos;s Classes
+             </Link>
+             <Link className='p-3 rounded-md bg-lamaPurpleLight' href="/">
+                 Teacher&apos;s Students           
+             </Link>
+             <Link className='p-3 rounded-md bg-lamaYellowLight' href="/">
+                 Teacher&apos;s Lessons
+             </Link>
+             <Link className='p-3 rounded-md bg-pink-50' href="/">
+                 Teacher&apos;s Exams
+             </Link>
+             <Link className='p-3 rounded-md bg-lamaSkyLight' href="/" >
+                 Teacher&apos;s Assignments
+             </Link>
+           </div>
+          </div>
+          <Performance />
+          <Announcements />
+        </div>   
     </div>
   )
 }
